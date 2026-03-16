258fd5d2ab72195060dc0b3a6bf5668b9723a42c
Joel Riquelme <joel.riquelme@ug.uchile.cl>
2026-03-16 17:18:54 -0300

    feat(usuario.py): mod listarTareas method

--- usuario.py
+++ usuario.py
@@ -7,3 +7,9 @@ class Usuario:
 
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
+
+    def listarTareas(self):
+        for tarea in self.tareas:
+            if tarea.estaLista():
+                print(f"La tarea {tarea.obtenerNombre()} está lista")
+                print(f"La tarea {tarea.obtenerNombre()} no está lista")
